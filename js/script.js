1986613f4be407caa617da7d020ae5d4a09b9a43
NikolaGolubovic <nikolagolubovic985@gmail.com>
2021-10-31 12:41:59 +0100

    move script  to namespace

--- js/script.js
+++ js/script.js
@@ -1,121 +1,158 @@
-const labelOpeners = document.querySelectorAll(".label-container p");
-const storageName = "trello-func-";
-
-const storage = JSON.parse(localStorage.getItem(storageName));
-const lists = document.querySelectorAll(".list");
-
-if (!storage) {
-  localStorage.setItem(storageName, JSON.stringify([]));
-} else {
-  storage.forEach((elem) => {
-    const newItem = document.createElement("div");
-    newItem.classList.add("item");
-    newItem.textContent = elem.value;
-    newItem.setAttribute("draggable", true);
-    newItem.dataset.id = elem.itemId;
-    lists.forEach((list) => {
-      if (list.dataset.id === elem.listId) {
-        list.children[1].appendChild(newItem);
-      }
-    });
-  });
-}
-
-labelOpeners.forEach((opener) =>
-  opener.addEventListener("click", function () {
-    this.parentElement.classList.toggle("active");
-  })
-);
-
-const btnsConfirm = document.querySelectorAll(".btn-add");
-
-btnsConfirm.forEach((btn) =>
-  btn.addEventListener("click", function () {
-    const storage = JSON.parse(localStorage.getItem(storageName));
-    const value = this.previousElementSibling.value;
-    const listId = this.parentElement.parentElement.parentElement.dataset.id;
-    itemId = storage.length + 1;
-    const newItemInfo = { value, listId, itemId };
-    storage.push(newItemInfo);
-    localStorage.setItem(storageName, JSON.stringify(storage));
-    const newItem = document.createElement("div");
-    newItem.classList.add("item");
-    newItem.textContent = value;
-    newItem.setAttribute("draggable", true);
-    newItem.dataset.id = itemId;
-    newItem.ondragstart = function (e) {
-      draggedItem = e.target;
-      setTimeout(function () {
-        e.target.style.display = "none";
-      }, 0);
-    };
-    newItem.ondragend = function (e) {
-      console.log("jap");
-      setTimeout(function () {
-        e.target.style.display = "block";
-        draggedItem = null;
-      }, 0);
-    };
-    this.parentElement.parentElement.parentElement.children[1].appendChild(
-      newItem
+const namespace = (function () {
+  const labelOpeners = document.querySelectorAll(".label-container p");
+  const storageName = "trello-func-";
+  const storage = JSON.parse(localStorage.getItem(storageName));
+  const lists = document.querySelectorAll(".list");
+  const btnsConfirm = document.querySelectorAll(".btn-add");
+  let draggedItem = null;
+  let items;
+  let labelOpen = function () {
+    return labelOpeners.forEach((opener) =>
+      opener.addEventListener("click", function () {
+        this.parentElement.classList.toggle("active");
+      })
     );
-    // blah
-  })
-);
-
-const items = document.querySelectorAll(".item");
-const itemsList = document.querySelectorAll(".list");
-
-let draggedItem = null;
-
-for (let i = 0; i < items.length; i++) {
-  const item = items[i];
-
-  item.addEventListener("dragstart", function () {
-    draggedItem = item;
-    setTimeout(function () {
-      item.style.display = "none";
-    }, 0);
-  });
-
-  item.addEventListener("dragend", function () {
-    setTimeout(function () {
-      draggedItem.style.display = "block";
-      draggedItem = null;
-    }, 0);
-  });
-
-  for (let j = 0; j < itemsList.length; j++) {
-    const list = itemsList[j];
-
-    list.addEventListener("dragover", function (e) {
-      e.preventDefault();
-    });
-
-    list.addEventListener("dragenter", function (e) {
-      e.preventDefault();
-      this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
-    });
-
-    list.addEventListener("dragleave", function (e) {
-      this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-    });
-
-    list.addEventListener("drop", function (e) {
-      const storage = JSON.parse(localStorage.getItem(storageName));
-      console.log(draggedItem);
-      this.children[1].append(draggedItem);
-      const newElem = {
-        value: draggedItem.textContent,
-        listId: this.dataset.id,
-        itemId: draggedItem.dataset.id,
-      };
-      const newStorage = storage.filter((elem) => {
-        return elem.itemId != draggedItem.dataset.id;
+  };
+  const buttonsToConfirmNewItems = function () {
+    btnsConfirm.forEach((btn) =>
+      btn.addEventListener("click", function () {
+        const storage = JSON.parse(localStorage.getItem(storageName));
+        const value = this.previousElementSibling.value;
+        const listId =
+          this.parentElement.parentElement.parentElement.dataset.id;
+        itemId = storage.length + 1;
+        const newItemInfo = { value, listId, itemId };
+        storage.push(newItemInfo);
+        localStorage.setItem(storageName, JSON.stringify(storage));
+        const newItem = document.createElement("div");
+        newItem.classList.add("item");
+        newItem.textContent = value;
+        newItem.setAttribute("draggable", true);
+        newItem.dataset.id = itemId;
+        newItem.ondragstart = function (e) {
+          namespace.setDraggedItem(e.target);
+          setTimeout(function () {
+            e.target.style.display = "none";
+          }, 0);
+        };
+        newItem.ondragend = function (e) {
+          setTimeout(function () {
+            e.target.style.display = "block";
+            namespace.setDraggedItem(null);
+          }, 0);
+        };
+        this.parentElement.parentElement.parentElement.children[1].appendChild(
+          newItem
+        );
+        namespace.createItems();
+        namespace.createDraggable();
+        console.log("alo");
+      })
+    );
+  };
+  const createStorage = function () {
+    if (!storage) {
+      localStorage.setItem(storageName, JSON.stringify([]));
+    } else {
+      storage.forEach((elem) => {
+        const newItem = document.createElement("div");
+        newItem.classList.add("item");
+        newItem.textContent = elem.value;
+        newItem.setAttribute("draggable", true);
+        newItem.dataset.id = elem.itemId;
+        lists.forEach((list) => {
+          if (list.dataset.id === elem.listId) {
+            list.children[1].appendChild(newItem);
+          }
+        });
       });
-      newStorage.push(newElem);
-      localStorage.setItem(storageName, JSON.stringify(newStorage));
-      this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
-    });
-  }
-}
+    }
+  };
+  const createItems = function () {
+    items = document.querySelectorAll(".item");
+  };
+  const getItems = function () {
+    return items;
+  };
+  const getDraggedItem = function (value) {
+    draggedItem = value;
+  };
+  const setDraggedItem = function (value) {
+    draggedItem = value;
+  };
+  const createDraggable = function () {
+    const items = this.getItems();
+    const itemsList = document.querySelectorAll(".list");
+
+    draggedItem = this.getDraggedItem();
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+
+      item.addEventListener("dragstart", function () {
+        draggedItem = item;
+        setTimeout(function () {
+          item.style.display = "none";
+        }, 0);
+      });
+
+      item.addEventListener("dragend", function () {
+        draggedItem.style.display = "block";
+        setTimeout(function () {
+          draggedItem = null;
+        }, 0);
+      });
+
+      for (let j = 0; j < itemsList.length; j++) {
+        const list = itemsList[j];
+
+        list.addEventListener("dragover", function (e) {
+          e.preventDefault();
+        });
+
+        list.addEventListener("dragenter", function (e) {
+          e.preventDefault();
+          this.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+        });
+
+        list.addEventListener("dragleave", function (e) {
+          this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+        });
+
+        list.addEventListener("drop", function (e) {
+          const storage = JSON.parse(localStorage.getItem(storageName));
+          this.children[1].append(draggedItem);
+          const newElem = {
+            value: draggedItem.textContent,
+            listId: this.dataset.id,
+            itemId: draggedItem.dataset.id,
+          };
+          const newStorage = storage.filter((elem) => {
+            return elem.itemId != draggedItem.dataset.id;
+          });
+          newStorage.push(newElem);
+          localStorage.setItem(storageName, JSON.stringify(newStorage));
+          this.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+        });
+      }
+    }
+  };
+  return {
+    labelOpen,
+    createStorage,
+    createItems,
+    createDraggable,
+    getItems,
+    getDraggedItem,
+    setDraggedItem,
+    buttonsToConfirmNewItems,
+  };
+})();
+
+window.addEventListener("load", function () {
+  namespace.labelOpen();
+  namespace.buttonsToConfirmNewItems();
+  namespace.createStorage();
+  namespace.createItems();
+  namespace.createDraggable();
+});
